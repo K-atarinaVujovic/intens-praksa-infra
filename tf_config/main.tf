d4b5dc53513df4d3dcf5e32634c281babca95bd1
Katarina Vujovic <KVujovic@livingston.com>
2026-07-09 16:06:55 +0200

    Add prometheus stack

--- tf_config/main.tf
+++ tf_config/main.tf
@@ -4,3 +4,10 @@ resource "helm_release" "intens" {
   namespace = "default"
   values    = [file("${path.module}/../intens/values.yaml")]
 }
+
+resource "helm_release" "prometheus" {
+  name       = "prometheus-stack"
+  namespace  = "monitoring"
+  chart      = "oci://ghcr.io/prometheus-community/charts/kube-prometheus-stack"
+  values    = [file("${path.module}/../intens/monitoring-values.yaml")]
+}
